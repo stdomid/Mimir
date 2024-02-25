73881b0bc880d170164530843e8e78a5c7a10c80
stdomid <ranjbaromidd@gmail.com>
2024-02-25 11:21:34 -0800

    Fix a bug in port scan and write robotsContent function

--- lib/mimirLib.py
+++ lib/mimirLib.py
@@ -20,8 +20,8 @@ class infoG:
         except socket.herror:
             return "No domain name found for the given IP address."
 
-    def portScan(target_host, target_ports) -> object:
-        for port in target_ports:
+    def portScan(target_host) -> object:
+        for port in [21,22,23,25,53,80,443,3070,5060,135,139,1433,1521,1723,1900,2302,3389,3306,4000,4444,5000,5555,5900,6667,6697,8000,8081,9100,9090,5900,445,5985,598]:
             try:
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 sock.settimeout(1)
@@ -51,6 +51,21 @@ class infoG:
                     print(Fore.GREEN+"[+] http://"+url+"/"+pay)
                 else:
                     print(Fore.RED+"[-] http://"+url+"/"+pay)
+
+    def robotsContent(url) -> object:
+        if url != "":
+            req = get("https://"+url+"/robots.txt")
+            if req.status_code == 200:
+                print("\nresults for this url http://"+url+"/robots.txt :\n")
+                print(req.text)
+            else:
+                print("\nSomething's wrong !\n")
+                print("Check your url")  
+                print("Check your internet connection")  
+        else:
+            print("This url does not have robots.txt file")     
+
+
 
 
 
